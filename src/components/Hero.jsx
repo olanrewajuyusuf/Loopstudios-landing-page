67c0d26cb805a10ad2c549ea5dc0795e4532c275
Wallewdev <wallewdev@gmail.com>
2023-10-24 09:32:24 +0100

    Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,10 @@ const Hero = () => {
         src="/images/mobile/image-hero.jpg" 
         alt="Hero" 
         />
-        <h1 className="hidden md:block absolute text-6xl text-white border-2 top-[35%] left-[11%] py-10 pl-10 pr-16 font-light">IMMERSIVE <br />EXPERIENCES <br />THAT DELIEVERS</h1>
+        <h1 className="hidden md:block absolute text-6xl text-white border-2 top-[35%] left-[11%] py-10 pl-10 pr-16 font-light">IMMERSIVE <br />EXPERIENCES <br />THAT DELIVER</h1>
 
         {/* ======Mobile view====== */}
-        <h1 className="md:hidden absolute w-[90%] text-4xl text-white border-2 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-5 font-light">IMMERSIVE <br />EXPERIENCES <br />THAT DELIEVERS</h1>
+        <h1 className="md:hidden absolute w-[90%] text-4xl text-white border-2 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-5 font-light">IMMERSIVE <br />EXPERIENCES <br />THAT DELIVER</h1>
     </div>
   )
 }
